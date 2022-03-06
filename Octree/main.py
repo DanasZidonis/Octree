62aec38f0453aa35adf580e53c69fb1b9f3917f3
DC_Danas <danas@dreamcubator.club>
2022-03-06 20:37:42 +0200

    Changed las file directory.

--- Octree/main.py
+++ Octree/main.py
@@ -2,7 +2,7 @@ import open3d as o3d
 import laspy
 import os
 
-inFile = laspy.read(os.getcwd() + r'\venv\LasFile\2743_1234.las')
+inFile = laspy.read(os.getcwd() + r'\2743_1234.las')
 pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(inFile.xyz)
 pcd.paint_uniform_color([124, 252, 0])
