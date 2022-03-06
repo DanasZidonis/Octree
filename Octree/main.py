d598eb87f5eed8084e9c9aeb9cd6780375283b07
DC_Danas <danas@dreamcubator.club>
2022-03-06 20:38:34 +0200

    Changed color.

--- Octree/main.py
+++ Octree/main.py
@@ -5,7 +5,7 @@ import os
 inFile = laspy.read(os.getcwd() + r'\2743_1234.las')
 pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(inFile.xyz)
-pcd.paint_uniform_color([124, 252, 0])
+pcd.paint_uniform_color([0.5, 1, 0])
 
 octree = o3d.geometry.Octree(max_depth=10)
 octree.convert_from_point_cloud(pcd, size_expand=0.01)
