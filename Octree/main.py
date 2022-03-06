8b10e060a01561a03acac44a704bde8e3c5a8864
DC_Danas <danas@dreamcubator.club>
2022-03-06 20:50:34 +0200

    Changed file read.

--- Octree/main.py
+++ Octree/main.py
@@ -2,7 +2,9 @@ import open3d as o3d
 import laspy
 import os
 
-inFile = laspy.read(os.getcwd() + r'\2743_1234.las')
+print('Enter las file location with name:')
+fileLocation = input()
+inFile = laspy.read(fileLocation)
 pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(inFile.xyz)
 pcd.paint_uniform_color([0.5, 1, 0])
